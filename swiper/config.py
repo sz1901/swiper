37921a18a4533727afb48d00593aeb4d41f77ad3
Michael Xu <380982916@qq.com>
2019-06-25 16:33:57 +0800

    1, 登录注册功能. 2,获取用户资料. 3, 编辑用户资料. 4, 上传用户头像.

---
+++ swiper/config.py
@@ -0,0 +1,18 @@
+"""存放第三方配置"""
+
+YZX_URL = 'https://open.ucpaas.com/ol/sms/sendsms'
+
+YZX_PARAMS = {
+ "sid":"442234d98d86a3413382b6aaf0211bfe",
+ "token":"f81d1ce2210bccaf90f5d9f9b87ae3a6",
+ "appid":"833b086cb62f40e4937cb7bef7e96c58",
+ "templateid":"448535",
+ "param": None,
+ "mobile": None,
+}
+
+
+ACCESS_KEY = 'uTCi4T1X2ozCDwQvzyEjCszNsATntmvKv-8ntFh0'
+SECRET_KEY = 'utEyFUPxY1C_wy8O2k3zbSnUteDhwWbigAGP4VOP'
+BUCKET_NAME = 'sz1901'
+QINIU_URL= 'http://ptnay1ksd.bkt.clouddn.com'
--- swiper/settings.py
+++ swiper/settings.py
@@ -25,30 +25,29 @@ SECRET_KEY = 'akzgof!aqy5$^x7@bt3jai^$j#f@57t!4+mnek_t--&(^4yi^('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'lib.middleware.AuthMiddleware'
 ]
-
+# from django.contrib.sessions.middleware import SessionMiddleware
 ROOT_URLCONF = 'swiper.urls'
 
 TEMPLATES = [
@@ -75,8 +74,12 @@ WSGI_APPLICATION = 'swiper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'swiper',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': 3306,
+        'HOST': '127.0.0.1',
     }
 }
 
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = 'medias'
